edd34a388f947e63b283f389407ac8002bc02e5d
Onishi Ryosuke <onishi.appcreate@gmail.com>
2021-01-10 17:51:16 +0900

    カウントダウン機能実装
    カウントダウンの機能と０秒になったら画面遷移する機能の実装

--- CountDown/ViewController.swift
+++ CountDown/ViewController.swift
@@ -9,8 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
       
+    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var startButton: UIButton!
+    
+    var time = 5
+    var timer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        countLabel.text = String(time)
+    }
+    
+    // ボタンが押された時の処理
+    @IBAction func buttonAction(_ sender: Any) {
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { (timer) in
+            self.time -= 1
+            self.countLabel.text = String(self.time)
+            
+            if self.time == 0 {
+                self.performSegue(withIdentifier: "next", sender: nil)
+            }
+        })
     }
 }
 
